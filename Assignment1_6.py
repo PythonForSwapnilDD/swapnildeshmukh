5b4ca1ca4a4fb0c293636c04b3dc9c7eae8d4dcf
pythonforswapnildeshmukh <57282367+pythonforswapnildeshmukh@users.noreply.github.com>
2019-11-08 20:11:10 +0530

    Assignment Added

---
+++ Assignment1_6.py
@@ -0,0 +1,18 @@
+def ChkNum(a):
+    if a > 0:
+        print("Number is positive")
+    elif a < 0:
+        print("Number is  negative")
+    elif a == 0:
+        print("Number is  zero")
+
+
+a = int(input("Enter the  number"))
+ChkNum(a)
+
+
+
+
+
+
+
