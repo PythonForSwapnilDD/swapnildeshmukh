aedb26d441ca7f6e63837f0de76aef87cb1d219a
pythonforswapnildeshmukh <57282367+pythonforswapnildeshmukh@users.noreply.github.com>
2019-11-08 20:13:47 +0530

    Assignment Added

---
+++ Assignment1_7.py
@@ -0,0 +1,9 @@
+def Num(p):
+    if (p % 5 == 0):
+        print("Number is divisible by 5")
+    else:
+        print("Number is  not divisible by 5")
+
+
+p = int(input("Enter the  number"))
+Num(p)
